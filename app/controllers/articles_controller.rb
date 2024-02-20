0f272c79bd887b73ebef80083d46e5c9e63ad838
Jacek Bastin <jacek.bastin@gmail.com>
2024-02-20 16:05:20 +0000

    Added Create and listing on index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+  before_action :prepare_data
+
+
   def index
     @articles = Article.all
   end
@@ -8,6 +11,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :author, :content)
+  end
+
+  def prepare_data
+    @article = Article.find(params[:id]) if (params[:id] != nil)
+
+  end
 
 end
